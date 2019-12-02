94b969ff4aaaa12f17367bdeb4e5c6522972eb55
Michael <michael.mhanna86@gmail.com>
2019-12-02 14:22:26 -0500

    APIkey still not working

--- script.js
+++ script.js
@@ -1,16 +1,86 @@
-var key = "3689129273ba0cb220b574b2f2631334";
+var key = "27643ed14a014e6ae93bb7e28d2eeb94";
+var city = "";
+var queryUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "appid=" + key
+var searchMethod = "city";
 var tempUnit = "metric";
-var searchMethod;
-//$(document).ready(); 
+$(document).ready(); 
 
+ function searchWeather(searchTerm){
+ fetch(`http://api.openweathermap.org/data/2.5/forecast?${searchMethod}=${searchTerm}&key=${key}&tempUnit=${tempUnit}`).then(result => {
+    return result.json();
+}).then(result => {
+    init(result);
+})
+ }
+//  $.ajax({
+//  url: queryUrl,
+//      Method: "GET"
+//  }).then(function(results){
+//      console.log(results);
+//  });
 
-$.ajax({
-    url: "http://api.openweathermap.org/data/2.5/forecast?",
-    method: "GET"
-}).then(function(response){
-    console.log(response);
-});
+function init(resultFromServer) {
+    switch (resultFromServer.weather[0].main) {
+        case 'Clear':
+            document.body.style.backgroundImage = "url('clear.jpg')";
+            break;
+        
+        case 'Clouds':
+            document.body.style.backgroundImage = "url('cloudy.jpg')";
+            break;
 
-function searchWeather(){
+        case 'Rain':
+        case 'Drizzle':
+        case 'Mist':
+            document.body.style.backgroundImage = "url('rain.jpg')";
+            break;
+        
+        case 'Thunderstorm':
+            document.body.style.backgroundImage = "url('stormy.jpg')";
+            break;
+        
+        case 'Snow':
+            document.body.style.backgroundImage = "url('snowy.jpg')";
+            break;
 
+        default:
+            break;
+    }
+
+    var weatherDiscription = $("weather-discrip-header");
+    var tempelement = $('#temp');
+    var humidity = $("humidity");
+    var wind = $("wind");
+    var uvIndex = $("uv-index");
+    var cityHeader = $("city-header");
+    var weatherImg = $("document-img");
+    var resultDiscription = resultFromServer.weather[0].description;
+    weatherDiscription.innertext = resultDiscription.charAt(0).upperCase() + resultDiscription.slice(1);
+    tempelement.innerHTML = resultFromServer.main.temp;
+    wind.innerHTML = " wind at " + resultFromServer.wind.speed;
+    cityHeader.innerHTML = resultFromServer.name;
+    humidity.innerHTML = " Humidity Levels at " + resultFromServer.main.humidity;
+    uvIndex.innerHTML = " UV Index at " + resultFromServer
+
+    weatherImg.src = "http://openweathermap.org/img/wn/" + resultFromServer.weather[0].icon + ".pmg";
 }
+
+    function infoBoxPosition(){
+        var weatherContainer = $("#weather-container");
+        var infoBoxH = weatherContainer.clientHeight;
+        var infoBoxW = weatherContainer.clientWidth;
+
+        wweatherContainer.style.left = `calc(50% - ${infoBoxH/2}px)`;
+        weatherContainer.style.top = `calc(50% - ${infoBoxW/1.3}px)`;
+        weatherContainer.style.visibility = 'visible';
+    }
+
+
+document.getElementById("search-button").addEventListener("click", () => {
+var searchTerm = document.getElementById("search-input").value;
+
+if(searchTerm)
+searchWeather(searchTerm);
+})
+
+
